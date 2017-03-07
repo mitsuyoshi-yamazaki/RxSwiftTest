b6c229d3e020acbf5ea3ae64763e092402084558
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-03-07 13:45:19 +0900

    temp commit

--- RxSwiftTest/Playground.playground/Contents.swift
+++ RxSwiftTest/Playground.playground/Contents.swift
@@ -5,3 +5,40 @@ import RxSwift
 
 var str = "Hello, playground"
 print(str)
+
+typealias StrClosure = (String) -> Void
+
+class ObjectToDeinit {
+  
+  let bag = DisposeBag()
+
+  init(_ observable: Observable<String>) {
+    print(#function)
+    
+    
+    
+    observable.subscribe(onNext: { [weak self] (str) in
+      print("onNext: \(str)")
+      self?.printText(str)
+    }, onError: { (error) in
+      print("onError: \(error)")
+    }).addDisposableTo(bag)
+  }
+  
+  deinit {
+    print(#function)
+  }
+  
+  // MARK: - 
+  private func printText(_ text: String) {
+    print("\(#function): \(text)")
+  }
+}
+
+let subject = PublishSubject<String>()
+let observable = subject.asObservable()
+
+var obj: ObjectToDeinit? = ObjectToDeinit(observable)
+obj = nil
+
+subject.onNext("🍎")
